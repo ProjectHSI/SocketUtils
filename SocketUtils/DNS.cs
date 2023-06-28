f58bd0f069ab1042e82c12ef3c9eef283cb5499a
Project_HSI <57279339+ProjectHSI@users.noreply.github.com>
2023-06-28 14:12:44 +1000

    initial

---
+++ SocketUtils/DNS.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Author: [Project HSI]
+ * Description: Adds DNS helper functions.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace SocketUtils
+{
+	public static class DNS
+	{
+		public static bool HostValid(string host)
+		{
+			try
+			{
+				Dns.GetHostEntry(host).AddressList[0].ToString();
+				return true;
+			}
+			catch (IndexOutOfRangeException)
+			{
+				return true;
+			}
+			catch (Exception)
+			{
+				return false;
+			}
+		}
+
+		public static string ResolveHost(string host)
+		{
+			try
+			{
+				return Dns.GetHostEntry(host).AddressList[0].ToString();
+			}
+			catch (IndexOutOfRangeException)
+			{
+				// probably already an IP address.
+				return host;
+			}
+			catch (Exception)
+			{
+				throw new ArgumentException($"{host} is not a valid IP address.");
+			}
+		}
+	}
+}
---
+++ SocketUtils/Socket.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SocketUtils
+{
+	public static class Socket
+	{
+		// we need to force the namespace here, otherwise this will read as SocketUtils->Socket->ConnectionOpen(SocketUtils->Socket)
+		// which is not valid, primarily due to the function getting it's parent (which is not acceptable here)
+		// or as the IDE puts it: putting a static class as a type (also not acceptable).
+		public static bool IsConnectionOpen(System.Net.Sockets.Socket socket)
+		{
+			try
+			{
+				/*
+				 * to fix ambiguity, we put the !socket.Poll... negative operation inside of brackets.
+				 * to prevent interpretations such as:
+				 * NOT(X AND Y)
+				 * and to force the interpretation as
+				 * (NOT(X)) AND Y 
+				*/
+				return ((!socket.Poll(100, SelectMode.SelectRead)) && (socket.Available == 0)) || !socket.Connected;
+			}
+			catch
+			{
+				return false;
+			}
+		}
+	}
+}
